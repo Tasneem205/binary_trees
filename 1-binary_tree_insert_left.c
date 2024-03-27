f60df37ada84a77e0e6bc4324a6facdddfe2a5c7
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-28 01:51:15 +0200

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert - Insert at the left a binary node
+ * binary_tree_insert_left - Insert at the left a binary node
  * @parent: node parent
  * @value: Value of the node
  * Return: the new node

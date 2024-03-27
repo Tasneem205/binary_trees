696dfcf81b12eefed63d77e0974138f11cf7f040
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-28 01:59:26 +0200

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,19 +9,19 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *newNode;
+	binary_tree_t *newNode;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
 	newNode = binary_tree_node(parent, value);
-        if (parent->right != NULL)
-        {
-                newNode->right = parent->right;
-                parent->right->parent = newNode;
-        }
-        parent->right = newNode;
-        return (newNode);
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+	return (newNode);
 }

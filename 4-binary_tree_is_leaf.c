330a2ce14663d672f76239549867640ef8c2ad83
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-28 02:08:11 +0200

    task 3 & 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - finds if the node is leaf
+ * @node: checks this node
+ * Returns: true or false
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->parent != NULL && (node->left == NULL && node->right == NULL))
+	{
+		return (1);
+	}
+	return (0);
+}
